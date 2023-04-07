1cdb1dc6db17866ca53c1baaf4ac7c4f7fda4c0e
abdulhamidongithub <1997abdulhamid@gmail.com>
2023-04-07 22:21:13 +0300

    Models done

--- asosiy/models.py
+++ asosiy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Mijoz(models.Model):
+    ism = models.CharField(max_length=50)
+    tugilgan_yil = models.PositiveSmallIntegerField()
+    vaqt = models.DateTimeField(auto_now_add=True)
+    tel_raqam = models.CharField(max_length=20)
+    faol = models.BooleanField(default=True)
+    longlitude = models.CharField(max_length=30)
+    latitude = models.CharField(max_length=30)
+    def __str__(self):
+        return f"{self.ism} ({self.vaqt})"
